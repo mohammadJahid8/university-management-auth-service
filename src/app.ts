330876b6a9c213688c7c5ad5c5da1df25860f721
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-06-06 23:50:05 +0600

    Error handling completed

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors'
-import express, { Application, Response } from 'express'
+import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
 
@@ -10,14 +10,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // import routes
+
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import userRouter from './app/modules/users/users.route'
+import { UserRoutes } from './app/modules/users/user.route'
+// import ApiError from './Erros/ApiError'
 
 // all routes
-app.use('/api/v1/users/', userRouter)
+app.use('/api/v1/users/', UserRoutes)
 
 // default route
-app.get('/', async (req, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Welcome to the university server!')
 })
 
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,41 +1,48 @@
-import { NextFunction, Request, Response } from 'express'
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 import ApiError from '../../Erros/ApiError'
 import config from '../../config'
 import { IGenericErrorMessages } from '../../interfaces/error'
 import handleValidationError from '../../interfaces/handleValidationError'
+import { errLogger } from '../../shared/logger'
 
-const globalErrorHandler = (
-  err: any,
+const globalErrorHandler: ErrorRequestHandler = (
+  error: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  config.env === 'development'
+    ? console.log('From global error handler: ', error)
+    : errLogger.error('From global error handler: ', error)
+
   let statusCode = 500
   let message = 'Something went wrong'
   let errorMessages: IGenericErrorMessages[] = []
 
-  if (err?.name === 'ValidationError') {
-    const simplifyError = handleValidationError(err)
+  if (error?.name === 'ValidationError') {
+    const simplifyError = handleValidationError(error)
     statusCode = simplifyError.statusCode
     message = simplifyError.message
     errorMessages = simplifyError.errorMessages
-  } else if (err instanceof Error) {
-    message = err?.message
-    errorMessages = err?.message
-      ? [{ path: 'error', message: err?.message }]
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [{ path: 'error', message: error?.message }]
       : []
-  } else if (err instanceof ApiError) {
-    statusCode = err.statusCode
-    message = err.message
-    errorMessages = err?.message
-      ? [{ path: 'error', message: err?.message }]
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error?.message
+    errorMessages = error?.message
+      ? [{ path: 'error', message: error?.message }]
       : []
   }
   res.status(statusCode).json({
     success: false,
     message,
     errorMessages,
-    stack: config.env === 'development' ? err.stack : undefined,
+    stack: config.env === 'development' ? error?.stack : undefined,
   })
 
   next()
