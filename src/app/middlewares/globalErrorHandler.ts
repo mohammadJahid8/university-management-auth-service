4d1a4b628a1c76e42f05f3e8c0561ecea178f5a0
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-06-07 00:23:58 +0600

    Removed unncessary files

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,10 +2,9 @@
 /* eslint-disable no-console */
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 import ApiError from '../../Erros/ApiError'
+import handleValidationError from '../../Erros/handleValidationError'
 import config from '../../config'
 import { IGenericErrorMessages } from '../../interfaces/error'
-import handleValidationError from '../../interfaces/handleValidationError'
-import { errLogger } from '../../shared/logger'
 
 const globalErrorHandler: ErrorRequestHandler = (
   error: any,
@@ -13,25 +12,28 @@ const globalErrorHandler: ErrorRequestHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  config.env === 'development'
-    ? console.log('From global error handler: ', error)
-    : errLogger.error('From global error handler: ', error)
+  // config.env === 'development'
+  //   ? console.log('From global error handler: ', error)
+  //   : errLogger.error('From global error handler: ', error)
 
   let statusCode = 500
   let message = 'Something went wrong'
   let errorMessages: IGenericErrorMessages[] = []
 
   if (error?.name === 'ValidationError') {
+    console.log('inside validation')
     const simplifyError = handleValidationError(error)
     statusCode = simplifyError.statusCode
     message = simplifyError.message
     errorMessages = simplifyError.errorMessages
   } else if (error instanceof Error) {
+    console.log('inside Error')
     message = error?.message
     errorMessages = error?.message
       ? [{ path: 'error', message: error?.message }]
       : []
   } else if (error instanceof ApiError) {
+    console.log('inside ApiError')
     statusCode = error?.statusCode
     message = error?.message
     errorMessages = error?.message
--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -3,6 +3,19 @@ import { UserService } from './user.service'
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
+    // req validation
+
+    // const createUserZodSchema = z.object({
+    //   body: z.object({
+    //     role: z.string({
+    //       required_error: 'Role is required',
+    //     }),
+    //     password: z.string().optional(),
+    //   }),
+    // })
+
+    // await createUserZodSchema.parseAsync(req)
+
     const { user } = req.body
     const result = await UserService.createUserService(user)
 
