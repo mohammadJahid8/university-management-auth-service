c72c69cc58ca7dd8d3c7de4584f33dfc5fee17a8
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-06-10 23:44:39 +0600

    academic pagination page and limit complete

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemesterService } from './academicSemester.service';
 
 const createSemester: RequestHandler = catchAsync(
@@ -18,6 +21,25 @@ const createSemester: RequestHandler = catchAsync(
   }
 );
 
+const getAllSemesters = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const paginationOptions = pick(req.query, paginationFields);
+
+    const result = await AcademicSemesterService.getAllSemesters(
+      paginationOptions
+    );
+    sendResponse<IAcademicSemester[]>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'All semesters got successfully!',
+      meta: result.meta,
+      data: result.data,
+    });
+    next();
+  }
+);
+
 export const AcademicSemesterController = {
   createSemester,
+  getAllSemesters,
 };
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,5 +1,7 @@
 import httpStatus from 'http-status';
 import ApiError from '../../../Erros/ApiError';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
@@ -15,6 +17,27 @@ const createSemester = async (
   return result;
 };
 
+const getAllSemesters = async (
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IAcademicSemester[]>> => {
+  const { page = 1, limit = 10 } = paginationOptions;
+
+  const skip = (page - 1) * limit;
+  const result = await AcademicSemester.find().sort().skip(skip).limit(limit);
+
+  const total = await AcademicSemester.countDocuments();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const AcademicSemesterService = {
   createSemester,
+  getAllSemesters,
 };
